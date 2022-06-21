284d5b6b70ae8e6bd0b4bf0a28d649fec1541a44
blarney2000 <topspinner1@hotmail.com>
2022-06-20 23:00:12 -0400

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ import './style.css';
 
 const pHashElement = document.getElementById('pHash');
 
+for (let i = 0; i < 64; i++) {
+  const cell = document.createElement('div');
+  cell.innerHTML = `<div style=" text-align: center; background-color: blue; color: gray; width: 22px; height: 22px;">${
+    i + 1
+  }</div>`;
+  pHashElement.append(cell);
+}
+
+
